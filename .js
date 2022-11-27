d304707489b4b09aeeb02d021c7fa9ef1185e470
David Reis <davisreis11@gmail.com>
2022-11-27 20:00:22 -0300

    project finished

---
+++ .js
@@ -0,0 +1,20 @@
+//visa
+//inicia com 4 seguido de mais 15 dígitos
+// ^4\d{0,15}
+//4234234234234234
+
+//master
+// inicia com 5, seguido de um digito entre 1 e 5, seguido de mais 2 dígitos
+// OU
+// inicia com 22, seguido de um dígito entre 2 e 9, seguido de mais 1 dígito
+// OU
+// inicia com 2, seguido de um dígito entre 3 e 7, seguido de mais dois dígitos
+//seguido de mais 12 dígitos
+//5353535353535353
+//2323232323232323
+//2232343526523421
+
+//(^5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\d{0,2})\d{0,12}
+
+
+//a2zsfer
